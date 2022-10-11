f3098cb4b23e2c12a392c2698a38c99f276bf00c
HuyTa <tadinhhuy907@gmail.com>
2022-10-11 23:47:12 +0700

    refactor: update uncontrolled component

--- src/pages/Uncontrolled/components/UncontrolledForm.tsx
+++ src/pages/Uncontrolled/components/UncontrolledForm.tsx
@@ -1,9 +1,16 @@
-import React, { useState, useRef, useMemo, useCallback, memo, FormEvent } from "react";
+import React, { useState, useRef, useCallback, memo, FormEvent } from 'react';
 import Input from '../../../components/Elements/Input';
 import { FormProps, SchemaForm } from '@/models/general';
 
+const initErrors = {
+  userName: '',
+  email: '',
+  password: '',
+  confirmPw: '',
+};
+
 const FormUnControlled: React.FC<FormProps> = ({ schemaForm, initValueForm }) => {
-  const [errors, setErrors] = useState<SchemaForm<string>>();
+  const [errors, setErrors] = useState<SchemaForm<string>>(initErrors);
   const userNameRef = useRef<HTMLInputElement | null>(null);
   const emailRef = useRef<HTMLInputElement | null>(null);
   const passwordRef = useRef<HTMLInputElement | null>(null);
@@ -12,124 +19,90 @@ const FormUnControlled: React.FC<FormProps> = ({ schemaForm, initValueForm }) =>
   const keyOfFormFields = Object.keys(initValueForm);
   const { userName, email, password, confirmPw } = schemaForm;
 
-  const formRef: { [key: string]: any } = useMemo(() => {
-    return {
-      userName: userNameRef,
-      email: emailRef,
-      password: passwordRef,
-      confirmPw: confirmPwRef
-    }
-  }, []);
+  const validateFormMessage = useCallback(
+    (formValues: SchemaForm<string>): SchemaForm<string> => {
+      const newMessages = keyOfFormFields.reduce((accumulator, currentKey) => {
+        const { regex, requiredMessage, errorMessage, validator, name } = schemaForm[currentKey];
 
-  const formValues = useMemo(() => {
-    const values = keyOfFormFields.reduce((accumulator, key) => {
-      return {
-        ...accumulator,
-        [key]: formRef?.[key]?.current?.value
-      };
-    }, {});
-    return values as SchemaForm;
-  }, [formRef, keyOfFormFields]);
+        const currentValue = formValues[currentKey];
+        const { password: passwordValue } = formValues;
 
-  const validateFormMessage = useCallback((): SchemaForm<string> => {
-    const newMessages = keyOfFormFields.reduce((accumulator, currentKey) => {
-      const {
-        regex,
-        requiredMessage,
-        errorMessage,
-        validator,
-        name
-      } = schemaForm[currentKey];
+        const currentParams =
+          name === 'confirmPw'
+            ? [passwordValue, currentValue, requiredMessage, errorMessage, regex]
+            : [currentValue, requiredMessage, errorMessage, regex];
 
-      const currentValue = formValues[currentKey];
-      const { password: passwordValue } = formValues;
+        const messages: string = validator(...currentParams);
 
-      const currentParams =
-        name === "confirmPw"
-          ? [passwordValue, currentValue, requiredMessage, errorMessage, regex]
-          : [currentValue, requiredMessage, errorMessage, regex];
+        return {
+          ...accumulator,
+          [currentKey]: messages,
+        };
+      }, {});
+      return newMessages as SchemaForm<string>;
+    },
+    [schemaForm, keyOfFormFields]
+  );
 
-      const messages: string = validator(...currentParams);
+  const checkValidate = useCallback(
+    (formValues: SchemaForm<string>) => {
 
-      return {
-        ...accumulator,
-        [currentKey]: messages
-      };
-    }, {});
-    return newMessages as SchemaForm<string>;
-  }, [formValues, schemaForm, keyOfFormFields]);
+      const areValidFields = keyOfFormFields.every((field) => formValues[field].trim());
+      console.log('areValidFields: ', areValidFields);
 
-  const checkValidate = useCallback(() => {
-    const values = formValues;
-    console.log('values: ', values);
+      const haveErrors = keyOfFormFields.some((field) => errors && errors[field]);
+      console.log('haveErrors: ', haveErrors);
 
-    const areInvalidFields = keyOfFormFields.every(
-      (field) => !values[field]?.trim()
-    );
-
-    const areInvalidErrors = keyOfFormFields.every(
-      (field) => errors && errors[field]
-    );
-
-    return areInvalidFields || areInvalidErrors;
-  }, [formValues, keyOfFormFields, errors]);
+      return !haveErrors && areValidFields;
+    },
+    [errors, keyOfFormFields]
+  );
 
   const onHandleSubmitForm = useCallback(
     (e: FormEvent) => {
       e.preventDefault();
-      if (checkValidate()) {
-        const messagesErrors = validateFormMessage();
-        setErrors(messagesErrors);
+      const formValues = {
+        userName: userNameRef.current?.value,
+        email: emailRef.current?.value,
+        password: passwordRef.current?.value,
+        confirmPw: confirmPwRef.current?.value,
+      };
+      console.log('formValues: ', formValues);
+      if (checkValidate(formValues as SchemaForm<string>)) {
+        console.log('Call API...', formValues);
         return;
-      }
-      console.log("Call API...", formValues);
+      };
+      const messagesErrors = validateFormMessage(formValues as SchemaForm<string>);
+      setErrors(messagesErrors);
     },
-    [checkValidate, formValues, validateFormMessage]
+    [checkValidate, validateFormMessage]
   );
 
   return (
-    <div style={{ height: "100%" }}>
+    <div style={{ height: '100%' }}>
       Form Uncontrolled:
       <form
         style={{
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center"
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
         }}
         onSubmit={onHandleSubmitForm}
       >
-        <div style={{ padding: "15px 0" }}>
-          <Input
-            field={userName}
-            errors={errors as SchemaForm<string>}
-            ref={userNameRef}
-          />
+        <div style={{ padding: '15px 0' }}>
+          <Input field={userName} errors={errors as SchemaForm<string>} ref={userNameRef} />
         </div>
-        <div style={{ padding: "15px 0" }}>
-          <Input
-            field={email}
-            errors={errors as SchemaForm<string>}
-            ref={emailRef}
-          />
+        <div style={{ padding: '15px 0' }}>
+          <Input field={email} errors={errors as SchemaForm<string>} ref={emailRef} />
         </div>
-        <div style={{ padding: "15px 0" }}>
-          <Input
-            field={password}
-            errors={errors as SchemaForm<string>}
-            ref={passwordRef}
-          />
+        <div style={{ padding: '15px 0' }}>
+          <Input field={password} errors={errors as SchemaForm<string>} ref={passwordRef} />
         </div>
-        <div style={{ padding: "15px 0" }}>
-          <Input
-            field={confirmPw}
-            errors={errors as SchemaForm<string>}
-            ref={confirmPwRef}
-          />
+        <div style={{ padding: '15px 0' }}>
+          <Input field={confirmPw} errors={errors as SchemaForm<string>} ref={confirmPwRef} />
         </div>
-        <button style={{ alignSelf: "center", marginTop: "25px" }}>
-          Submit
-        </button>
+        <button style={{ alignSelf: 'center', marginTop: '25px' }}>Submit</button>
       </form>
     </div>
   );
